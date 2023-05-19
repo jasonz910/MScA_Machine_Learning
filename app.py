dfc19bdface17d2b83cfe512951f292104a82630
jasonz910 <jasonn.zyj@gmail.com>
2023-05-19 16:36:24 -0500

    CHANGES

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import sys
 import os
 import pandas as pd 
 from keras_vggface.utils import preprocess_input
-from keras_vggface import VGGFace
-from keras.models import Model
+from keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img
 import warnings
 warnings.filterwarnings("ignore")
@@ -17,9 +16,15 @@ import av
 from PIL import Image
 import io
 
-svr_model = joblib.load('svr_model.pkl')
-vggface = VGGFace(model='vgg16', include_top=True, input_shape=(224, 224, 3), pooling='avg')
-vggface_model = Model(inputs=vggface.input, outputs=vggface.get_layer('fc6').output)
+@st.cache_data#(allow_output_mutation=True)
+def load_models():
+    vggface_model = load_model("vggface_model.h5")
+    svr_model = joblib.load("svr_model.pkl")
+    return vggface_model, svr_model
+
+vggface_model, svr_model = load_models()
+# vggface = VGGFace(model='vgg16', include_top=True, input_shape=(224, 224, 3), pooling='avg')
+# vggface_model = Model(inputs=vggface.input, outputs=vggface.get_layer('fc6').output)
 
 def get_fc6_feature(img):
     img = np.expand_dims(img, axis=0)
@@ -56,15 +61,15 @@ def prepare_download(img):
     image_bytes = buf.getvalue()
     return image_bytes
 
-# class VideoProcessor:
+class VideoProcessor:
 
-#     def recv(self, frame):
+    def recv(self, frame):
 
-#         frm = frame.to_ndarray(format = 'bgr24')
+        frm = frame.to_ndarray(format = 'bgr24')
 
-#         predict_bmi(frm)
+        predict_bmi(frm)
 
-#         return av.VideoFrame.from_ndarray(frm, format = 'bgr24') 
+        return av.VideoFrame.from_ndarray(frm, format = 'bgr24') 
     
 
 
@@ -77,55 +82,55 @@ st.caption('The BMI predicted from this site is based on learning your face feat
 
 upload, photo, live = st.tabs(['Upload Your Photo','Take a Photo','Video Live'])
 
-#with upload:
+with upload:
 
-upload_files = st.file_uploader("Upload your photo to predict:", accept_multiple_files=True)
+    upload_files = st.file_uploader("Upload your photo to predict:", accept_multiple_files=True)
 
-for upload_file in upload_files:
+    for upload_file in upload_files:
 
-    index = 1
+        index = 1
 
-    pic_upload = np.array(Image.open(upload_file))
+        pic_upload = np.array(Image.open(upload_file))
 
-    predict_bmi(pic_upload)
+        predict_bmi(pic_upload)
 
-    pil_pic_upload = Image.fromarray(pic_upload)
-    st.image(pil_pic_upload, use_column_width=True, clamp=True)
+        pil_pic_upload = Image.fromarray(pic_upload)
+        st.image(pil_pic_upload, use_column_width=True, clamp=True)
 
-    pic_download = prepare_download(pil_pic_upload)
-
-    st.download_button(
-        label="Download Prediction",
-        data=pic_download,
-        file_name='Image'+str(index)+'_with_BMI.jpg',
-        mime='image/jpeg',
-    )
+        pic_download = prepare_download(pil_pic_upload)
     
-    index += 1
+        st.download_button(
+            label="Download Prediction",
+            data=pic_download,
+            file_name='Image'+str(index)+'_with_BMI.jpg',
+            mime='image/jpeg',
+        )
+        
+        index += 1
 
 
-# with photo:
+with photo:
 
-#     picture = st.camera_input("Take a photo to predict:")
+    picture = st.camera_input("Take a photo to predict:")
 
-#     if picture is not None:
+    if picture is not None:
 
-#         picture_taken = np.array(Image.open(picture))
+        picture_taken = np.array(Image.open(picture))
 
-#         predict_bmi(picture_taken)
+        predict_bmi(picture_taken)
 
-#         pil_pic_taken = Image.fromarray(picture_taken)
-#         st.image(pil_pic_taken, use_column_width=True, clamp=True)
+        pil_pic_taken = Image.fromarray(picture_taken)
+        st.image(pil_pic_taken, use_column_width=True, clamp=True)
 
-#         photo_download = prepare_download(pil_pic_taken)
+        photo_download = prepare_download(pil_pic_taken)
 
-#         st.download_button(
-#             label="Download Prediction",
-#             data=photo_download,
-#             file_name='Photo_with_BMI.jpg',
-#             mime='image/jpeg',
-#         )
+        st.download_button(
+            label="Download Prediction",
+            data=photo_download,
+            file_name='Photo_with_BMI.jpg',
+            mime='image/jpeg',
+        )
 
-# with live:
+with live:
 
-#     webrtc_streamer(key="example", video_transformer_factory=VideoProcessor, sendback_audio=False)
+    webrtc_streamer(key="example", video_transformer_factory=VideoProcessor, sendback_audio=False)
