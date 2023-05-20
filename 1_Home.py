83fbedec78563bf239a06fcb485b3e544d34e9b5
jasonz910 <jasonn.zyj@gmail.com>
2023-05-19 23:06:49 -0500

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -56,7 +56,7 @@ st.write('👈👈👈Or you can predict your BMI from your face using three way
 st.markdown("""
     <style>
     .footer {
-    position: static;
+    position: relative;
     left: 0;
     bottom: 0;
     width: 100%;
